e4d30f3e8fd014a71ab8ce807208e80f46f2ebac
yangxi <yangxi@deepexi.com>
2021-03-16 09:44:12 +0800

    mq消费方业务逻辑处理与ack要用try catch进行处理

--- demo-spring-boot-rabbitmq/demo-spring-boot-rabbitmq-amqp-consumer/src/main/java/com/yx/rabbitmq/amqp/consumer/mq/OrderMQListener.java
+++ demo-spring-boot-rabbitmq/demo-spring-boot-rabbitmq-amqp-consumer/src/main/java/com/yx/rabbitmq/amqp/consumer/mq/OrderMQListener.java
@@ -1,6 +1,7 @@
 package com.yx.rabbitmq.amqp.consumer.mq;
 
 import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -11,9 +12,11 @@ import java.io.IOException;
 /**
  * 订单消息消费方监听器
  */
+// @RabbitListener直接放在方法上即可，不需要放在类上，参考：https://blog.csdn.net/koubi1234/article/details/89676010
+// @RabbitListener(queues = RabbitMQConfig.QUEUE)
 @Component
+@Slf4j
 public class OrderMQListener {
-
 
     @RabbitListener(queues = RabbitMQConfig.QUEUE)
     @RabbitHandler
@@ -24,16 +27,23 @@ public class OrderMQListener {
         System.out.println("message="+message.toString());
         System.out.println("body="+body);
 
-        //复杂业务逻辑
-        
+        try {
+            // try里面消费消息，执行本地业务方法，一般是要用一个本地事务保证原子性
+            //复杂业务逻辑，且要增加事务 TODO
 
-        //告诉broker，消息已经被确认
-        channel.basicAck(msgTag,false);
+            // 业务处理成功，告诉broker，消息已经被确认
+            channel.basicAck(msgTag,false);
 
-        //告诉broker，消息拒绝确认
-        //channel.basicNack(msgTag,false,true);
+        } catch(Exception e) {
 
-        //channel.basicReject(msgTag,true);
+            log.error("消费失败, msgTag={}", msgTag, e);
+
+            // 告诉broker，消息拒绝确认（可以设置批量ack）
+            //channel.basicNack(msgTag,false,true);
+
+            // 告诉broker，消息拒绝确认（一次只能拒绝一个消息，不可以设置批量ack）
+            //channel.basicReject(msgTag,true);
+        }
 
     }
 
